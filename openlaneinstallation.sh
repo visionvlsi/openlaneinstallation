9406cc15992403dd1faffd0eecab11be2f8df139
Vision-VLSI Industry PVT. LTD <vvi.pvtltd@gmail.com>
2022-09-17 08:42:11 +0530

    Update openlaneinstallation.sh

--- openlaneinstallation.sh
+++ openlaneinstallation.sh
@@ -111,12 +111,15 @@ echo "******************************** Cloning OpenLane and will start installat
 echo
 echo
 git clone https://github.com/The-OpenROAD-Project/OpenLane.git
-cd OpenLane
-sudo make openlane
+cd OpenLane/
+sudo make
+#git clone https://github.com/The-OpenROAD-Project/OpenLane.git
+#cd OpenLane
+#sudo make openlane
     # Default PDK_ROOT is $(pwd)/pdks. If you want to install the PDK at a differnt location, #uncomment the next line.
     #export PDK_ROOT=<absolute path to where skywater-pdk and open_pdks will reside>
-sudo make pdk
-sudo make test # This is to test that the flow and the pdk were properly installed
+#sudo make pdk
+#sudo make test # This is to test that the flow and the pdk were properly installed
 
 echo "********************************************* openSTA installation *********************************************"
 cd 
